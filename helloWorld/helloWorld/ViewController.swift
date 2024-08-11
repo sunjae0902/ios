e5ec87f61e616fbc8ac4a963e8141c6fa954a663
sunjae0902 <sunjaegg0902@naver.com>
2024-08-11 21:04:54 +0900

    implement a simple storyboard view
    - 커스텀 폰트 및 아이콘 추가
    - constraints 설정
    - outlet variable & action func 작성

--- helloWorld/helloWorld/ViewController.swift
+++ helloWorld/helloWorld/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        for family in UIFont.familyNames.sorted() {
+        //            let names = UIFont.fontNames(forFamilyName: family)
+        //            print("Family: \(family) Font names: \(names)")
+        //        }
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet var nickname: UITextField!
+    
+    @IBOutlet var introduce: UITextField!
+    
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+        bottomText.text = "Hello, I'm \(nickname.text!) \(introduce.text!)"
+    }
+    
+    @IBOutlet var bottomText: UILabel!
 }
+
+
 
