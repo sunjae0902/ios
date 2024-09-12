4ee7b9b1c2144ce121a6d715d5d80c0484e496b6
sunjae0902 <sunjaegg0902@naver.com>
2024-09-12 15:00:23 +0900

    add: swiftUI NavigationStack example
    apply routing pattern

---
+++ swift_ui/navigation/navigation/navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationApp.swift
+//  navigation
+//
+//  Created by sunjae on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
